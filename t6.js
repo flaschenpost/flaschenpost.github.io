6dbba479c29b4385af8e300f0558eaa4c47886df
marco <mg@cantamen.de>
2024-11-25 21:56:08 +0100

    temp

---
+++ t6.js
@@ -0,0 +1,51 @@
+class SimpleExtension {
+    constructor(){
+    }
+    getInfo() {
+        return {
+            "id": "simpleextension",
+            "name": "Simple Extension",
+            "blocks": [
+                {
+                    "opcode": "logVariables",
+                    "blockType": "command",
+                    "text": "log variables with prefix [TEXT]",
+                    "arguments": {
+                        "TEXT": {
+                            "type": "string",
+                            "defaultValue": "Debug:"
+                        }
+                    }
+                }
+            ]
+        };
+    }
+
+    logVariables(args) {
+        const prefix = args.TEXT;
+        console.log(prefix, "Starting variable dump");
+        
+        // Get all targets (sprites + stage)
+        const targets = vm.runtime.targets;
+        
+        // For each target, log its variables
+        targets.forEach(target => {
+            console.log(prefix, "---", target.getName(), "---");
+            const variables = target.variables;
+            Object.values(variables).forEach(variable => {
+                console.log(prefix, variable.name, "=", variable.value);
+            });
+        });
+    }
+}
+
+Scratch.extensions.register(new SimpleExtension());
+
+/*
+// Register the extension
+(function() {
+    var extensionInstance = new SimpleExtension(window.vm.extensionManager.runtime)
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+})()
+*/
